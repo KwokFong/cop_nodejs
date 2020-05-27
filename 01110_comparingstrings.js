14c70b1a35600165f3be8a5b4d7d1e7cee5d8f1e
Kwok Fong <kwokfong@gmail.com>
2020-05-26 21:06:09 -0700

    Adding 01110_comparingstrings.js

---
+++ 01110_comparingstrings.js
@@ -0,0 +1,19 @@
+// https://www.youtube.com/watch?v=m4915XyRV7c
+'use strict';
+
+let text = 'cat';
+
+console.log('text =', text, "\n");
+
+console.log("text == 'cat'", text == 'cat');        // Equality
+console.log("text === 'cat'", text === 'cat');      // Identity; Preferred
+
+console.log("text != 'cat'", text != 'cat');
+console.log("text !== 'cat'", text !== 'cat');
+
+console.log("text < 'dog'", text < 'dog');
+console.log("text <= 'dog'", text <= 'dog');
+
+console.log("text > 'dog'", text > 'dog');
+console.log("text >= 'dog'", text >= 'dog');
+console.log("'Dog' >= 'dog'", 'Dog' >= 'dog');
