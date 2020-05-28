8ed2abee4f34c4712ca23971dc825e9c1e414c08
Kwok <kwokfonggit@gmail.com>
2020-05-28 14:03:44 -0700

    Adding 01120_identity.js

---
+++ 01120_identity.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let days = 7;
+console.log('days =', days, "\n");
+
+// Equality Test Operator
+console.log("days == '7'", days == '7');    // Auto Converted to number.
+
+// Identity Test Operator
+console.log("days === '7'", days === '7');  // No Conversion; Much Stricter; Test Both Equality & Type
+
+console.log("days != '7'", days != '7');
+console.log("days !== '7'", days !== '7');  
+
+
+
